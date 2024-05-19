7919830ff916e501e635202512f1c6ea7f663f42
Watch-dog-s <126652678+Watch-dog-s@users.noreply.github.com>
2024-05-19 17:52:35 +0300

    fragments

--- electronic_diary/app/src/main/java/com/example/electronic_diary/StudentActivity.java
+++ electronic_diary/app/src/main/java/com/example/electronic_diary/StudentActivity.java
@@ -1,17 +1,21 @@
 package com.example.electronic_diary;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class StudentActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_student);
@@ -20,5 +24,23 @@ public class StudentActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+
+        findViewById(R.id.buttonMarks).setOnClickListener(this::Marks);
+
+    }
+
+
+
+
+    protected void Marks(View view)
+    {
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment1, MarkFragment.class, null).commit();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        MarkFragment fragment1 = new MarkFragment();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.container, fragment1, "fragment1");
+        fragmentTransaction.commit();
     }
 }
